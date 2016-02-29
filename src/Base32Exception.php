370cb26e36a61f87ffa5b764c2d9f2a603672e18
smiley <smiley@chillerlan.net>
2016-02-29 17:01:46 +0100

    added exception placeholder

---
+++ src/Base32Exception.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Class Base32Exception
+ *
+ * @filesource   Base32Exception.php
+ * @created      29.02.2016
+ * @package      chillerlan\Base32
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+
+namespace chillerlan\Base32;
+
+use Exception;
+
+/**
+ * 
+ */
+class Base32Exception extends Exception{
+
+}

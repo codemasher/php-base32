8ced3ef5fb5ac71bd2560f75553e54f8c2f8ce5e
smiley <smiley@chillerlan.net>
2016-02-29 17:16:31 +0100

    prepared tests

---
+++ tests/Base32Test.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ *
+ * @filesource   Base32Test.php
+ * @created      29.02.2016
+ * @author       Smiley <smiley@chillerlan.net>
+ * @copyright    2015 Smiley
+ * @license      MIT
+ */
+class Base32Test extends \PHPUnit_Framework_TestCase{
+
+	public function testInstance(){
+
+	}
+
+}
